9c02234d610b6c65ca30195c9686785167e75f56
FabioGadegbeku <fabio.gadegbeku.etu@univ-lille.fr>
2023-11-12 02:28:03 +0100

    fix

--- sim_based_cs/c_scores.py
+++ sim_based_cs/c_scores.py
@@ -1,10 +1,8 @@
+"""This module contains the functions to compute the constraint scores of a dataset"""
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.metrics.pairwise import rbf_kernel
 
-def laplacian_score(X):
+def laplacian_score(x):
     """Computes the laplacian score of a dataset
 
     Args:
@@ -14,15 +12,15 @@ def laplacian_score(X):
         laplacian_score : laplacian score of the data set
     """
     # Compute the similarity matrix
-    S = rbf_kernel(X)
+    simalarity_matrix = rbf_kernel(x)
     # Compute the degree matrix
-    D = np.diag(S.sum(axis=1))
+    degree_matrix = np.diag(simalarity_matrix.sum(axis=1))
     # Compute the lapacien matrix
-    L = D - S
+    laplacian_matrix = degree_matrix - simalarity_matrix
     # center the data
-    X = X - X.mean(axis=0)
+    x = x - x.mean(axis=0)
     # Compute the lapacien score for each feature
-    laplacian_score_num = np.diag(np.dot(X.T, np.dot(L, X)))
-    laplacian_score_den = np.diag(np.dot(X.T, np.dot(D, X)))
-    laplacian_score = laplacian_score_num / laplacian_score_den
-    return laplacian_score
+    laplacian_score_num = np.diag(np.dot(x.T, np.dot(laplacian_matrix, x)))
+    laplacian_score_den = np.diag(np.dot(x.T, np.dot(degree_matrix, x)))
+    laplacian_score_matrix = laplacian_score_num / laplacian_score_den
+    return laplacian_score_matrix
