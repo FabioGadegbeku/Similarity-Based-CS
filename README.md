889929bc9e140a252b1f4fad028b9ab31eb544b4
FabioGadegbeku <fabio.gadegbeku.etu@univ-lille.fr>
2023-11-23 11:09:01 +0100

    added new functions for constraint score 4

--- README.md
+++ README.md
@@ -5,15 +5,13 @@ This project addresses the curse of dimensionality in feature selection methods,
 
 - Curse of Dimensionality: The project tackles the challenges associated with high-dimensional data and the impact on machine learning tasks.
 
-- Feature Selection: Describes the process of selecting a subset of relevant features to improve model efficiency and effectiveness.
+- Feature Selection: Describes the process of selecting a subset of relevant features to improve model effectiveness.
 
 - Constraint Scores: Explains the use of must-link and cannot-link constraints and their limitations when evaluating individual features.
 
 - Correlations Between Features: Highlights the importance of considering feature interactions and dependencies in real-world data.
 
-- High-Dimensional Original Feature Space: Discusses the issues associated with working in high-dimensional spaces and its impact on similarity computation.
-
-- New Constraint Score: Introduces the primary focus of the project, which is the development of a novel constraint score that operates on feature subsets in a lower-dimensional subspace.
+- New Constraint Score: A novel constraint score that operates on feature subsets in a lower-dimensional subspace.
 
 ## Code Quality :
 ![Alt text](./coverage.svg)
--- sim_based_cs/c_scores.py
+++ sim_based_cs/c_scores.py
@@ -6,7 +6,7 @@ def laplacian_score(x):
     """Computes the laplacian score of a dataset
 
     Args:
-        X : data set withour labels, column index, or column names
+        X : data set without labels
 
     Returns:
         laplacian_score : laplacian score of the data set
@@ -24,3 +24,67 @@ def laplacian_score(x):
     laplacian_score_den = np.diag(np.dot(x.T, np.dot(degree_matrix, x)))
     laplacian_score_matrix = laplacian_score_num / laplacian_score_den
     return laplacian_score_matrix
+
+
+def get_constraints(x):
+    """ gets the constraints from the dataset
+
+    Args:
+        x : data set with available labels
+    Returns:
+        must_link (Must Link Matrix): i,j = 1 if i and j are must link, 0 otherwise
+        cannot_link (Cannot Link Matrix): i,j = 1 if i and j are cannot link, 0 otherwise
+    """
+    classe = x[:, -1]
+    must_link = np.zeros((len(classe), len(classe)))
+    cannot_link = np.zeros((len(classe), len(classe)))
+    for i in range(len(classe)):
+        for j in range(len(classe)):
+            if classe[i] == classe[j] :
+                must_link[i, j] = 1
+            if classe[i] != classe[j] and np.isnan(classe[i]) == False and np.isnan(classe[j]) == False:
+                cannot_link[i, j] = 1
+    return must_link, cannot_link
+
+
+## function that generates p constraints by setting n-p targets to NaN randomly and returns the target with n-p nan values
+def generate_constraints(x, p):
+    """ generates p constraints by setting n-p targets to NaN randomly and returns the target with n-p nan values
+    Args : x : data set with available labels
+              p : number of constraints
+    Returns : target : target with n-p nan values
+    """
+    target = x[:, -1]
+
+    # Randomly select n-p indices to set to nan
+    nan_indices = np.random.choice(target.size, target.size - p, replace=False)
+
+    # Set selected indices to nan
+    target = target.astype(np.float64)
+    target[nan_indices] = np.nan
+
+    return target
+
+
+def constraint_score_1(x,target):
+    """Computes the constraint score 1 of a dataset
+
+    Args:
+        x : data set without labels
+        target : available labels
+
+    Returns:
+        constraint_score_1 : constraint score 1 of the data set
+    """
+    # Get the constraints
+    must_link, cannot_link = get_constraints(target)
+
+    degree_matrix_must_link = np.diag(must_link.sum(axis=1))
+    degree_matrix_cannot_link = np.diag(cannot_link.sum(axis=1))
+    laplacian_matrix_must_link = degree_matrix_must_link - must_link
+    laplacian_matrix_cannot_link = degree_matrix_cannot_link - cannot_link
+
+    constraint_score_1_num = np.diag(np.dot(x.T, np.dot(laplacian_matrix_must_link, x)))
+    constraint_score_1_den = np.diag(np.dot(x.T, np.dot(laplacian_matrix_cannot_link, x)))
+    constraint_score_1 = constraint_score_1_num / constraint_score_1_den
+    return constraint_score_1
