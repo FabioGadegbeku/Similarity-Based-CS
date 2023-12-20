c1d9231ce60b0718442978c26826c0e3c4b099a1
FabioGadegbeku <fabio.gadegbeku.etu@univ-lille.fr>
2023-12-20 14:04:38 +0100

    feat : new functions

--- sim_based_cs/c_scores.py
+++ sim_based_cs/c_scores.py
@@ -1,7 +1,7 @@
 """This module contains the functions to compute the constraint scores of a dataset"""
 import numpy as np
 from sklearn.metrics.pairwise import rbf_kernel
-
+from sklearn.preprocessing import StandardScaler
 def laplacian_score(x):
     """Computes the laplacian score of a dataset
 
@@ -11,14 +11,15 @@ def laplacian_score(x):
     Returns:
         laplacian_score : laplacian score of the data set
     """
+    # Center and normalize the data
+    scaler = StandardScaler()
+    scaler.fit_transform(x)
     # Compute the similarity matrix
     simalarity_matrix = rbf_kernel(x)
     # Compute the degree matrix
     degree_matrix = np.diag(simalarity_matrix.sum(axis=1))
     # Compute the lapacien matrix
     laplacian_matrix = degree_matrix - simalarity_matrix
-    # center the data
-    x = x - x.mean(axis=0)
     # Compute the lapacien score for each feature
     laplacian_score_num = np.diag(np.dot(x.T, np.dot(laplacian_matrix, x)))
     laplacian_score_den = np.diag(np.dot(x.T, np.dot(degree_matrix, x)))
@@ -88,3 +89,44 @@ def constraint_score_1(x,target):
     constraint_score_1_den = np.diag(np.dot(x.T, np.dot(laplacian_matrix_cannot_link, x)))
     constraint_score_1 = constraint_score_1_num / constraint_score_1_den
     return constraint_score_1
+
+
+def similarity_matrix_score(x,m):
+    """Computes the similarity matrix score of a dataset keeping the m first features
+
+    Args:
+        x : data set with available labels
+
+    Returns:
+        The similarity matrix score for each feature
+    """
+    target = x[:, -1]
+    x = x[:, :-1]
+    # Center and normalize the data
+    scaler = StandardScaler()
+    scaler.fit_transform(x)
+    # Compute the similarity matrix for each feature
+    similarity_matrix_score =[]
+    #get the constraints
+    must_link, cannot_link = get_constraints(target)
+    for i in range(m+1,x.shape[1]-1):
+        similarity_matrix = (rbf_kernel(x[:,m:i].reshape(-1, 1)))
+        similarity_matrix_supervised = np.zeros((len(target), len(target)))
+        for j in range(len(target)):
+            for k in range(len(target)):
+                if must_link[j, k] == 1:
+                    similarity_matrix_supervised[j, k] = 1
+                if cannot_link[j, k] == 1:
+                    similarity_matrix_supervised[j, k] = 0
+                else:
+                    similarity_matrix_supervised[j, k] = similarity_matrix[j, k]
+        similarity_matrix_score.append(np.linalg.norm(similarity_matrix_supervised - similarity_matrix))
+
+    return similarity_matrix_score
+
+
+
+
+    # Compute the similarity matrix score for each feature with constraints
+
+    return list_similarity_matrix
--- tests/test_score.py
+++ tests/test_score.py
@@ -1,2 +1,52 @@
+""" This Module contains the tests for the c_score module  """
+import pytest
+import numpy as np
+import os
+import sys
+file_dir = os.path.dirname('Similarity-Based-CS')
+sys.path.append(file_dir)
+from sim_based_cs.c_scores import *
+
 def test_basic():
     assert 1 == 1
+
+def test_laplacian_score():
+    """ This function tests the laplacian_score function """
+    # Generate a random dataset
+    x = np.random.rand(100, 10)
+    # Compute the laplacian score
+    laplacian_score_matrix = laplacian_score(x)
+    # Check if the laplacian score matrix is a numpy array
+    assert isinstance(laplacian_score_matrix, np.ndarray)
+    # Check if the laplacian score matrix is of the right shape
+    assert laplacian_score_matrix.shape == (10,)
+
+def test_get_constraints():
+    """ This function tests the get_constraints function """
+    x = np.full((5, 5), 1)
+    # Compute the constraints
+    must_link, cannot_link = get_constraints(x)
+    # Check if the results are the right shape
+    assert must_link.shape == (5, 5)
+    assert cannot_link.shape == (5, 5)
+    # check if we get the correct results
+    assert np.all(must_link == np.full((5, 5), 1))
+    assert np.all(cannot_link == np.full((5, 5), 0))
+
+
+def test_generate_constraints():
+    """ This function tests the generate_constraints function """
+    # Generate a random dataset
+    x = np.random.rand(100, 10)
+    # Generate 10 constraints
+    target = generate_constraints(x, 10)
+    # Check if the result is a numpy array
+    assert isinstance(target, np.ndarray)
+    # Check if the result is of the right shape
+    assert target.shape == (100,)
+    # Check if the result contains the right number of nan values
+    assert np.sum(np.isnan(target)) == 90
+
+
+
+
